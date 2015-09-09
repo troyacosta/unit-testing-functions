d095e3377d4b0aa82462f5de267ad889f6328ffb
Troy Acosta <troyacosta@sbcglobal.net>
2015-09-09 17:27:00 -0500

    push

--- tests/getMonth.test.js
+++ tests/getMonth.test.js
@@ -6,4 +6,7 @@ describe('getMonth', function() {
 	it('should exist', function() {
 		expect(functions.getMonth).not.to.be.undefined;
 	});
+	it('should throw an exception if the argument passed in is not an integer', function() {
+		expect(function() {functions.getMonth(1) }).to.throw('Invalid Input');
+	});
 });
